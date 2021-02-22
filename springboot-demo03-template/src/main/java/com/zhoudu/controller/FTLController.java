61cf5c57e04eae1416cc0d9bb32c69f9929b1c59
zhouduforgit <1974788603@qq.com>
2021-02-22 14:47:29 +0800

    第三次次提交，增加了yml配置代替以前properties文件，加入一些FreeMarker模板页面

---
+++ springboot-demo03-template/src/main/java/com/zhoudu/controller/FTLController.java
@@ -0,0 +1,16 @@
+package com.zhoudu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class FTLController {
+    @RequestMapping("/ftl")
+    public String index(Model model){
+        model.addAttribute("now",new Date().toString());
+        return "index";
+    }
+}
---
+++ springboot-demo03-template/src/main/resources/templates/index.ftl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        Hi,FreeMark, 现在时间是${now}
+    <br>
+    ${"Hi Freemarker,现在时间是 ${now} !"}
+    <br>
+
+    </body>
+</html>
