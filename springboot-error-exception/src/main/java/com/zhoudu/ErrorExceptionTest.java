5de5586137682a26c46eb5fd44a595937efb5b13
zhouduforgit <1974788603@qq.com>
2021-03-04 21:06:41 +0800

    自定义错误页面的四种方式

---
+++ springboot-error-exception/src/main/java/com/zhoudu/ErrorExceptionTest.java
@@ -0,0 +1,13 @@
+package com.zhoudu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorExceptionTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorExceptionTest.class, args);
+    }
+
+}
---
+++ springboot-error-exception/src/test/java/com/zhoudu/SpringbootErrorExceptionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zhoudu;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
